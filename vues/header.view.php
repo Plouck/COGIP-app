1377e9ecd5fd39715df16b547575044055e273df
Alex <infanti.alexandre@gmail.com>
2018-08-31 11:46:50 +0200

    push maj

--- vues/header.view.php
+++ vues/header.view.php
@@ -4,28 +4,29 @@
     <meta charset="utf-8">
     <title>Accueil</title>
 
+      <link rel="stylesheet" href="../assets/CSS/style.css">
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-      <link rel="stylesheet" href="/assets/CSS/style.css">
+
   </head>
 
   <body>
 
 <header>
-  <div class="container">
-    <div class="row">
-      <div class="col-md-2">
-        <img class="float-left" src="assets/image/COGIP.png" alt="">
+    <div class="row" id="RAGE">
+      <div class="col-md-5">
+        <figure>
+          <img src="assets/image/COGIP.png" alt="">
+        </figure>
       </div>
-      <div class="col-md-8">
-        COGIP C LA VI TAVU
+      <div class="col-md-5">
+        <p>COGIP C LA VI TAVU</p>
       </div>
       <div class="col-md-2">
-        <form class="float-right" action="controllers/header.php" method="post">
-          <input type="text" name="log" value="Login">
-          <input type="text" name="mdp" value="Mot de passe">
+        <form action="controllers/header.php" method="post">
+          Login : <input type="text" name="log" value="">
+          Mot de passe : <input type="password" name="mdp" value="">
           <input type="submit" name="send" value="Envoi">
         </form>
       </div>
     </div>
-  </div>
 </header>
