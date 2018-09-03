f6e7491898e3456ce3afb2eb3be93f571b8dc054
MCambie <cambie.marie@gmail.com>
2018-09-03 15:47:00 +0200

    fin des pages avec detail au clique

---
+++ controllers/detailfacture.controllers.php
@@ -0,0 +1,6 @@
+<?php
+require "../models/detailfacture.models.php";
+
+$donnees = getDetailBill();
+
+ ?>
--- vues/detailfacture.view.php
+++ vues/detailfacture.view.php
@@ -1,6 +1,20 @@
 <?php
-include "header.view.php";
+require "header.view.php";
+require "../controllers/detailfacture.controllers.php";
  ?>
+ <h1>Société</h1>
+ <table>
+   <tr>
+     <th>N° de Facture</th>
+     <th>Date</th>
+     <th>Détails</th>
+     <th>Société</th>
+     <th>Personne de contact</th>
+   </tr>
+ <?php
+ echo "<tr><td>".  $donnees[0]['idFacture' ]. "</td><td>". $donnees[0]['date']."</td><td>". $donnees[0]['details']."</td><td>". $donnees[0]['societe_idSociete']."</td><td>". $donnees[0]['personne_idPersonne']."</td></tr>";
+ ?>
+ </table>
  <?php
  include "footer.view.php";
   ?>
