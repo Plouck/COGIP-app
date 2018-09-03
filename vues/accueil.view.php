7bed11129d62f3491c77452cd423ed8185418766
Poyraz Hakan <hakan.poyraz1994@gmail.com>
2018-09-03 10:44:53 +0200

    avancement

--- vues/accueil.view.php
+++ vues/accueil.view.php
@@ -1,44 +1,50 @@
 
 
       <!-- Card pour les 3 données demander -->
-      <div class="card text-center">
-        <div class="card-header">
-          <ul class="nav nav-tabs card-header-tabs">
-            <li class="nav-item">
-              <a class="nav-link" href="#">Factures</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Personnes</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Entreprises</a>
-            </li>
-          </ul>
+        <div class="row">
+          <div class="col-sm-4">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title align">Factures</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-4">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Personnes</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-4">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Entreprises</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+            </div>
+          </div>
         </div>
-        <div class="card-body">
-          <h5 class="card-title">Special title treatment</h5>
-
-          <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-      </div>
-
       <!-- double card pour consulter les clients ou les fournisseurs -->
       <div class="row">
         <div class="col-sm-6">
           <div class="card">
             <div class="card-body">
-              <h5 class="card-title">Special title treatment</h5>
-              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
+              <h2 class="card-title">Les clients</h2>
+              <a href="#" class="btn btn-primary">Afficher tout les clients</a>
             </div>
           </div>
         </div>
         <div class="col-sm-6">
           <div class="card">
             <div class="card-body">
-              <h5 class="card-title">Special title treatment</h5>
-              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
+              <h2 class="card-title">Les fournisseurs</h2>
+              <a href="#" class="btn btn-primary">Afficher tout les fournisseurs</a>
             </div>
           </div>
         </div>
