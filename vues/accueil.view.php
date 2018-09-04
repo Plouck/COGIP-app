ebf5d39fe2ac96bdc61de0050dc8a86e554a769e
Alex <infanti.alexandre@gmail.com>
2018-09-04 14:21:39 +0200

    pull de la master 14h21

--- vues/accueil.view.php
+++ vues/accueil.view.php
@@ -6,8 +6,8 @@
             <div class="card">
               <div class="card-body">
                 <h5 class="card-title text-center">Factures</h5>
-                <p class="card-text"> <?php require "models\accueilFactures.model.php"?> </p>
-                <input class="btn btn-primary" name="" type="button" onclick="self.location.href='?page=factures'" />
+                <p class="card-text"> <?php require "models/accueilFactures.model.php"?> </p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
               </div>
             </div>
           </div>
@@ -15,8 +15,8 @@
             <div class="card">
               <div class="card-body">
                 <h5 class="card-title text-center">Personnes</h5>
-                <p class="card-text"><?php require "models\accueilPersonnes.model.php"?></p>
-                <input class="btn btn-primary" name="" type="button" onclick="self.location.href='?page=clients'" />
+                <p class="card-text"><?php require "models/accueilPersonnes.model.php"?></p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
               </div>
             </div>
           </div>
@@ -24,8 +24,8 @@
             <div class="card">
               <div class="card-body">
                 <h5 class="card-title text-center">Entreprises</h5>
-                <p class="card-text"><?php require "models\accueilSociete.model.php"?></p>
-                <input class="btn btn-primary" name="" type="button" onclick="self.location.href='?page=societe'" />
+                <p class="card-text"><?php require "models/accueilSociete.model.php"?></p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
               </div>
             </div>
           </div>
@@ -36,26 +36,16 @@
         <div class="col-sm-6">
           <div class="card">
             <div class="card-body">
-<<<<<<< HEAD
               <h5 class="card-title">Les clients</h5>
               <a href="#" class="btn btn-primary">Afficher tout les clients</a>
-=======
-              <h2 class="card-title">Les clients</h2>
-              <a onclick="self.location.href='?page=annuaire'" class="btn btn-primary">Afficher tout les clients</a>
->>>>>>> hakan
             </div>
           </div>
         </div>
         <div class="col-sm-6">
           <div class="card">
             <div class="card-body">
-<<<<<<< HEAD
               <h5 class="card-title">Les fournisseurs</h5>
               <a href="#" class="btn btn-primary">Afficher tout les fournisseurs</a>
-=======
-              <h2 class="card-title">Les fournisseurs</h2>
-              <a onclick="self.location.href='?page=fournisseurs'" class="btn btn-primary">Afficher tout les fournisseurs</a>
->>>>>>> hakan
             </div>
           </div>
         </div>
